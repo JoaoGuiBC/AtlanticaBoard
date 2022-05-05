6e18d2dc7122e0c825610d362b66a72ca0d0007b
joaoguibc <joaoguibc@gmail.com>
2022-05-05 16:37:57 -0300

    Feature: SidebarDrawerContext

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -2,15 +2,20 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 import { Dashboard } from '../pages/dashboard';
 import { SignIn } from '../pages/signIn';
+import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
 
 export function Router() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/signIn" />} />
-        <Route path="/signIn" element={<SignIn />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <SidebarDrawerProvider>
+        <Routes>
+          <Route path="/" element={<Navigate to="/signIn" />} />
+          <Route path="/signIn" element={<SignIn />} />
+          <Route path="/signIn/test" element={<SignIn />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/users" element={<Dashboard />} />
+        </Routes>
+      </SidebarDrawerProvider>
     </BrowserRouter>
   );
 }
