24bcf95cb9efc776bcca0bed304fa40bdec2963e
joaoguibc <joaoguibc@gmail.com>
2022-05-05 01:58:27 -0300

    Feature: Created SignInPage

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { Router } from './routes';
+import { theme } from './styles/theme';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Router />
     </ChakraProvider>
   </React.StrictMode>,
--- web/src/pages/signIn.tsx
+++ web/src/pages/signIn.tsx
@@ -1,7 +1,34 @@
+import { Button, Flex, Stack } from '@chakra-ui/react';
+
+import { Input } from '../components/Form/Input';
+
 export function SignIn() {
   return (
-    <div>
-      <p>Hello signIn</p>
-    </div>
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={4}
+        flexDir="column"
+      >
+        <Stack spacing="8">
+          <Input type="email" name="email" label="E-mail" />
+          <Input type="password" name="password" label="Senha" />
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="blue"
+          size="lg"
+          borderRadius={4}
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
   );
 }
