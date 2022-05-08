b892e40275e3ec70da7843fd3ff1e59e5383238e
joaoguibc <joaoguibc@gmail.com>
2022-05-08 19:36:22 -0300

    Feature: EmployeeList page now have real data

--- web/src/pages/dashboard/employees/index.tsx
+++ web/src/pages/dashboard/employees/index.tsx
@@ -1,10 +1,13 @@
 import { RiDeleteBinLine } from 'react-icons/ri';
+import { useEffect, useState } from 'react';
+import { getDocs, collection, query } from 'firebase/firestore';
 import {
   Box,
   Button,
   Flex,
   Heading,
   Icon,
+  Spinner,
   Table,
   Tbody,
   Td,
@@ -15,16 +18,52 @@ import {
   useBreakpointValue,
 } from '@chakra-ui/react';
 
+import { firestore } from '../../../services/firebase';
+
 import { Header } from '../../../components/Header';
 import { Sidebar } from '../../../components/Sidebar';
 
-const employeesCount = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+type Employees = {
+  email: string;
+  name: string;
+  isAdmin: boolean;
+};
 
 export function EmployeeList() {
+  const [isFetchingEmployees, setIsFetchingEmployees] = useState(false);
+  const [didFetchFailed, setDidFetchFailed] = useState(false);
+  const [employees, setEmployees] = useState<Employees[]>([]);
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
   });
+
+  async function getEmployeesData() {
+    setIsFetchingEmployees(true);
+
+    try {
+      const employeesCollectionRef = collection(firestore, 'funcionarios');
+      const employeesDocsRef = query(employeesCollectionRef);
+      const employeesDocsSnaps = await getDocs(employeesDocsRef);
+
+      const parsedData = employeesDocsSnaps.docs.map(doc => ({
+        email: doc.data().email,
+        name: doc.data().name,
+        isAdmin: doc.data().isAdmin,
+      }));
+
+      setEmployees(parsedData);
+    } catch (_) {
+      setDidFetchFailed(true);
+    }
+
+    setIsFetchingEmployees(false);
+  }
+
+  useEffect(() => {
+    getEmployeesData();
+  }, []);
 
   return (
     <>
@@ -41,55 +80,67 @@ export function EmployeeList() {
               </Heading>
             </Flex>
 
-            <Table colorScheme="whiteAlpha">
-              <Thead>
-                <Tr>
-                  <Th>Nome</Th>
-                  <Th>E-Mail</Th>
-                  <Th width="8" />
-                </Tr>
-              </Thead>
-              <Tbody>
-                {employeesCount.map(key => (
-                  <Tr key={key}>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold" color="blue.400">
-                          Pessoa {key}
-                        </Text>
-                      </Box>
-                    </Td>
-
-                    <Td>
-                      <Text fontSize="sm" color="gray.300">
-                        joaoguibc@gmail.com
-                      </Text>
-                    </Td>
-
-                    <Td>
-                      <Button
-                        as="a"
-                        size="sm"
-                        fontSize="sm"
-                        borderRadius={4}
-                        colorScheme="red"
-                        leftIcon={
-                          isWideVersion && (
-                            <Icon as={RiDeleteBinLine} fontSize="16" />
-                          )
-                        }
-                      >
-                        {isWideVersion ? (
-                          'Excluir'
-                        ) : (
-                          <Icon as={RiDeleteBinLine} fontSize="16" />
-                        )}
-                      </Button>
-                    </Td>
+            {isFetchingEmployees ? (
+              <Flex justify="center">
+                <Spinner />
+              </Flex>
+            ) : didFetchFailed ? (
+              <Flex justify="center">
+                <Text>Falha ao obter os dados dos usuários.</Text>
+              </Flex>
+            ) : (
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th>Nome</Th>
+                    <Th>E-Mail</Th>
+                    <Th width="8" />
                   </Tr>
-                ))}
-              </Tbody>
-            </Table>
+                </Thead>
+                <Tbody>
+                  {employees.map(employee => (
+                    <Tr key={employee.email}>
+                      <Td>
+                        <Box>
+                          <Text fontWeight="bold" color="blue.400">
+                            {employee.name}
+                          </Text>
+                        </Box>
+                      </Td>
+
+                      <Td>
+                        <Text fontSize="sm" color="gray.300">
+                          {employee.email}
+                        </Text>
+                      </Td>
+
+                      <Td>
+                        {!employee.isAdmin && (
+                          <Button
+                            as="a"
+                            size="sm"
+                            fontSize="sm"
+                            borderRadius={4}
+                            colorScheme="red"
+                            leftIcon={
+                              isWideVersion && (
+                                <Icon as={RiDeleteBinLine} fontSize="16" />
+                              )
+                            }
+                          >
+                            {isWideVersion ? (
+                              'Excluir'
+                            ) : (
+                              <Icon as={RiDeleteBinLine} fontSize="16" />
+                            )}
+                          </Button>
+                        )}
+                      </Td>
+                    </Tr>
+                  ))}
+                </Tbody>
+              </Table>
+            )}
           </Box>
         </Flex>
       </Box>
